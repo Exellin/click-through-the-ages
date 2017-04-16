7829d64c20435de7d71be502529a3011a544a830
exellin <carnegieshawn@yahoo.ca>
2017-04-16 04:27:32 +0000

    added farm and passive tick rate to game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,93 @@
-var gameState = {
-    resources : { stone : 0, food : 0 }
-};
+var gameState = setDefaults();
+
+function setDefaults() {
+  var defaults = {
+    resources : { 
+      stone : {
+        name: "stone",
+        total: 0,
+        storage: 1000
+      },
+      food : {
+        name: "food",
+        total: 0,
+        storage: 1000
+      },
+      wood: {
+        name: "wood",
+        total: 0,
+        storage: 1000
+      }
+    },
+    buildings : {
+      farm: {
+        name: "farm",
+        total: 0,
+        wood_cost: 10,
+        food_rate: 1
+      }
+    }
+  };
+  return defaults;
+}
 
 window.setInterval(function() {
+  addResources();
   save();
+  updateDisplay();
 }, 1000);
 
 initialize();
 
 function initialize() {
   load();
-  updateDisplay();
 
   var resourceImageContainers = document.querySelectorAll(".resource_image_container");
   for(var i = 0; i < resourceImageContainers.length; i++) {
-    resourceImageContainers[i].addEventListener("click", clickEvents);
+    resourceImageContainers[i].addEventListener("click", resourceClickEvents);
+  }
+  
+  var buildingContainers = document.querySelectorAll(".building");
+  for (var i = 0; i < buildingContainers.length; i++) {
+    buildingContainers[i].addEventListener("click", purchaseBuilding);
   }
   
   document.querySelector("#save").addEventListener("click", save);
   document.querySelector("#delete").addEventListener("click", deleteSave);
 }
 
-function clickEvents() {
+function resourceClickEvents() {
+  var resourceName = this.id;
   animateResource(this);
-  incrementResource(this.id.substring(0, this.id.indexOf('_')));
+  incrementResource(gameState.resources[resourceName]);
+}
+
+function purchaseBuilding() {
+  var buildingName = this.id;
+  var canPurchase = true;
+  
+  Object.keys(gameState.resources).forEach(function(resourceName) {
+    var resourceCost = gameState.buildings[buildingName][resourceName + "_cost"];
+    var resourceTotal = gameState.resources[resourceName].total;
+    if (resourceCost && canPurchase === true) {
+      if (resourceTotal < resourceCost) {
+        canPurchase = false;
+      }
+    }
+  });
+  
+  if (canPurchase === true) {
+    Object.keys(gameState.resources).forEach(function(resourceName) {
+      var resourceCost = gameState.buildings[buildingName][resourceName + "_cost"];
+      if (resourceCost) {
+        gameState.resources[resourceName].total -= resourceCost;
+        gameState.buildings[buildingName][resourceName + "_cost"] += 10;
+        updateDisplay();
+      }
+    });
+    gameState.buildings[buildingName].total += 1;
+    updateDisplay();
+  }
 }
   
 function animateResource(resourceImageContainer) {
@@ -58,26 +122,30 @@ function animateResource(resourceImageContainer) {
 }
     
 function incrementResource(resource) {
-  gameState.resources[resource] += 1;
+  resource.total += 1;
   updateDisplay();
 }
 
 function updateDisplay() {
   Object.keys(gameState).forEach(function(category) {
-    loop(gameState[category]);
+    setProperty(gameState[category], "total");
+    setProperty(gameState[category], "storage");
+    setProperty(gameState[category], "wood_cost");
   });
-
-  function loop(category) {
+  
+  function setProperty(category, property) {
     Object.keys(category).forEach(function(key) {
-      document.querySelector("#" + key).textContent = category[key];
+      if (category[key][property] !== undefined) {
+        document.querySelector("#" + category[key].name + "_" + property).textContent = category[key][property];
+      }
     });
   }
 }
 
 function save() {
   try {
-    window.localStorage.setItem("save",JSON.stringify(gameState));
-    if (this.textContent === "Save Game") {
+    window.localStorage.setItem("save", JSON.stringify(gameState));
+    if (this.id === "save") {
       alert("Manually saved the game");
     }
   } catch(err) {
@@ -93,30 +161,36 @@ function load() {
   }
   
   if (savegame) {
-    Object.keys(gameState).forEach(function(category) {
-      setProperties(gameState[category], savegame[category]);
-    });
+    gameState = savegame;
   }
-  
-  function setProperties(category, savegameCategory) {
-    Object.keys(category).forEach(function(key) {
-      if (typeof(savegameCategory[key]) !== undefined)  category[key] = savegameCategory[key];
-    });
-  }
+  updateDisplay();
 }
 
 function deleteSave() {
   if (window.confirm("Are you sure you want to delete your save?")) {
     window.localStorage.removeItem("save");
-    Object.keys(gameState).forEach(function(category) {
-      setToZero(gameState[category]);
-    });
-    updateDisplay(gameState);
+    gameState = setDefaults();
+    updateDisplay();
   }
+}
+
+function addResources() {
+  Object.keys(gameState.resources).forEach(function (resourceName) {
+    if (gameState.resources[resourceName].total <= gameState.resources[resourceName].storage) {
+      gameState.resources[resourceName].total += calculateResourceRate(resourceName);
+    }
+  });
   
-  function setToZero(category) {
-    Object.keys(category).forEach(function(key) {
-      category[key] = 0;
+  function calculateResourceRate(resourceName) {
+    var resourcesPerTick = 0;
+    Object.keys(gameState.buildings).forEach(function (buildingName) {
+      if (gameState.buildings[buildingName][resourceName + "_rate"]) {
+        resourcesPerTick += gameState.buildings[buildingName][resourceName + "_rate"] * gameState.buildings[buildingName].total;
+      }
     });
+    if (resourcesPerTick > (gameState.resources[resourceName].storage - gameState.resources[resourceName].total)) {
+      resourcesPerTick = gameState.resources[resourceName].storage - gameState.resources[resourceName].total;
+    }
+    return resourcesPerTick;
   }
 }
